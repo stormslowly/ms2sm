4673ba1c555cab2f39aef813cb46f12203c8ef7f
stormslowly <stormslowly@gmail.com>
2014-01-05 21:54:14 +0800

    remveo uesless code

--- index.js
+++ index.js
@@ -4,69 +4,25 @@
 var sql = require("./sql.js");
 var mysql = require("mysql");
 
-
-
-
-
-
-var tableName = 'bus_vehicles';
-var pkQuery = "SHOW INDEX FROM " + tableName + ";";
-
-function __DESCRIBE__(err, schema, cb) {
-    if (err) {
-      console.log(err);
-      connection.end();
-      return;
-      
-    }
-
-    connection.query(pkQuery, function(err, pkResult) {
-      if(err){
-        return cb(err);
-      } 
-
-      // Loop through Schema and attach extra attributes
-      schema.forEach(function(attr) {
-
-        // Set Primary Key Attribute
-        if(attr.Key === 'PRI') {
-          attr.primaryKey = true;
-
-          // If also an integer set auto increment attribute
-          if(attr.Type === 'int(11)') {
-            attr.autoIncrement = true;
-          }
-        }
-
-        // Set Unique Attribute
-        if(attr.Key === 'UNI') {
-          attr.unique = true;
-        }
-      });
-
-      // Loop Through Indexes and Add Properties
-      pkResult.forEach(function(result) {
-        schema.forEach(function(attr) {
-          if( attr.Field !== result.Column_name){ 
-            return;
-          }
-          attr.indexed = true;
-        });
-      });
-
-      // Convert mysql format to standard javascript object
-      var normalizedSchema = sql.normalizeSchema(schema);
-
-      // Set Internal Schema Mapping
-      // dbs[collectionName].schema = normalizedSchema;
-
-      // TODO: check that what was returned actually matches the cache
-      // cb(null, normalizedSchema);
-      console.log(normalizedSchema);
-      connection.end();
-    });
-
+function __DESCRIBE__(err, schema) {
+  if (err) {
+    console.log(err);
+    connection.end();
+    return;
+    
   }
+
+  // Convert mysql format to standard javascript object
+  var normalizedSchema = sql.normalizeSchema(schema);
+
+  // Set Internal Schema Mapping
+  // dbs[collectionName].schema = normalizedSchema;
+
+  // TODO: check that what was returned actually matches the cache
+  // cb(null, normalizedSchema);
+  console.log(normalizedSchema);
+  connection.end();
+}
 
 
 var argvs = process.argv.slice(2);
